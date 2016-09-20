a00f792ec5d6bf3a1cda7a2f88a0d66f2b0ea7fb
John McDonald <jcpmcdonald@gmail.com>
2016-09-20 11:05:13 -0600

    Setting up some simple tests, and laying the groundwork for finding the best price

--- Blatant Shopping/BasicTests.cs
+++ Blatant Shopping/BasicTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace BlatantShopping
@@ -11,27 +12,40 @@ namespace BlatantShopping
 	internal class BasicTests
 	{
 		[Test]
-		public void LoadLists()
+		public void LoadPrices()
 		{
-			// Load a simple list
+			PriceService priceService = new PriceService();
 
-			// Load an out-of order list
+			// Load a simple list
+			var simplePriceList = priceService.GetPriceList(@"{ 'bananas': '0.75', 'apples': '1.75' }");
+			Assert.That(simplePriceList, Is.Not.Null);
+			Assert.That(simplePriceList["bananas"], Is.EqualTo(0.75m));
+			Assert.That(simplePriceList["apples"], Is.EqualTo(1.75m));
 
 			// Load an empty list
+			var emptyPriceList = priceService.GetPriceList(@"{ }");
+			Assert.That(emptyPriceList, Is.Not.Null);
+			Assert.That(emptyPriceList.Count, Is.EqualTo(0));
 
-			// Load a null list
+			// Load a blank string. Fail Fast
+			Assert.Throws<ArgumentException>(() => priceService.GetPriceList(@""));
 
-			// Load a list with duplicates
+			// Load a null. Fail Fast
+			Assert.Throws<ArgumentException>(() => priceService.GetPriceList(null));
 
-			// Load a list with out of order duplicates
-			Assert.That(false, Is.False, "Test");
+			// Load a list with duplicates, even though duplicate keys aren't technically allowed in JSON, and are strictly forbidden in a Dictionary<>
+			var duplicatePriceList = priceService.GetPriceList(@"{ 'bananas': '0.75', 'apples': '1.75' , 'bananas': '0.60'}");
+			Assert.That(duplicatePriceList, Is.Not.Null);
+			// The last value for bananas should take effect
+			Assert.That(duplicatePriceList["bananas"], Is.EqualTo(0.60m));
+
 		}
 
 
 		[Test]
 		public void LoadSales()
 		{
-			
+
 		}
 	}
 }
---
+++ Blatant Shopping/PriceService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BlatantShopping
+{
+	class PriceService
+	{
+
+		public Dictionary<String, decimal> GetPriceListFromFile(String filename)
+		{
+			return GetPriceList(File.ReadAllText(filename));
+		}
+
+		public Dictionary<String, decimal> GetPriceList(String json)
+		{
+			if (String.IsNullOrEmpty(json))
+			{
+				throw new ArgumentException("json can not be null or empty");
+			}
+			return JsonConvert.DeserializeObject<Dictionary<string, decimal>>(json);
+		}
+
+
+		public decimal GetPrice(Dictionary<String, int> shoppingList, Dictionary<String, decimal> priceList, Dictionary<String, List<ISale>> saleList)
+		{
+			decimal total = 0;
+
+			// Go through each item in the shopping list and get the best price
+			foreach (var item in shoppingList)
+			{
+				total += GetPrice(item.Key, item.Value, priceList, saleList);
+			}
+
+			return total;
+		}
+
+
+		public decimal GetPrice(String product, int quantity, Dictionary<String, decimal> priceList, Dictionary<String, List<ISale>> saleList)
+		{
+			// Look for any sales
+			decimal bestSalePrice = decimal.MaxValue;
+			var sales = saleList[product.ToLower()];
+			foreach (var sale in sales)
+			{
+				
+			}
+
+			// Look up the regular price
+			decimal regularPrice = priceList[product] * quantity;
+
+			// Return the cheapest price
+			return Math.Min(bestSalePrice, regularPrice);
+		}
+	}
+}
