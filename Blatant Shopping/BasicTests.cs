5a30d56e669b17c9a2aab5ae202bb04a027b6378
John McDonald <jcpmcdonald@gmail.com>
2016-09-20 09:51:37 -0600

    Starting with NUnit and Json.NET NuGets

---
+++ Blatant Shopping/BasicTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BlatantShopping
+{
+	[TestFixture]
+	internal class BasicTests
+	{
+		[Test]
+		public void LoadLists()
+		{
+			// Load a simple list
+
+			// Load an out-of order list
+
+			// Load an empty list
+
+			// Load a null list
+
+			// Load a list with duplicates
+
+			// Load a list with out of order duplicates
+			Assert.That(false, Is.False, "Test");
+		}
+
+
+		[Test]
+		public void LoadSales()
+		{
+			
+		}
+	}
+}
---
+++ Blatant Shopping/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlatantShopping
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
